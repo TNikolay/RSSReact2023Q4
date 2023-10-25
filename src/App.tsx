e5a0b522a59b3223894a4e315d569a42aaf8a873
Nikolay Tyushkov <begemot@begemotov.net>
2023-10-25 23:45:15 +0300

    feat: add search field component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css';
+import { Component } from 'react';
+import MainPage from './pages/MainPage';
 
-function App() {
-  return (
-    <>
-      <h1>Hi</h1>
-    </>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="pt-5 mx-auto w-[90%]">
+        <MainPage />
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import SearchBar from '../components/SearchBar';
+
+const LS_QUERY_ITEM_NAME = 'TN_Query';
+
+interface State {
+  query: string;
+}
+
+type Props = Record<string, never>;
+
+export default class MainPage extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { query: localStorage.getItem(LS_QUERY_ITEM_NAME) ?? '' };
+  }
+
+  onSearchSubmit = (query: string) => {
+    localStorage.setItem(LS_QUERY_ITEM_NAME, query);
+    this.setState({ ...this.state, query });
+  };
+
+  render() {
+    return (
+      <>
+        <h1 className="my-6 text-3xl text-center">Hi there!</h1>
+        <SearchBar query={this.state.query} onSubmit={this.onSearchSubmit} />
+        <p className="my-6 text-center">Query: `{this.state.query}`</p>
+      </>
+    );
+  }
+}
