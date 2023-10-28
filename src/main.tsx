b490ce61a42f865eaded183a4c17cd58a0ee34c2
Nikolay Tyushkov <begemot@begemotov.net>
2023-10-28 14:13:59 +0300

    feat: add Error Boundary

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import axios from 'axios';
+import ErrorBoundary from './components/ErrorBoundary';
 
 axios.defaults.baseURL = 'https://rickandmortyapi.com/api';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -6,6 +6,7 @@ const LS_QUERY_ITEM_NAME = 'TN_Query';
 
 interface State {
   query: string;
+  throwError: boolean;
 }
 
 type Props = Record<string, never>;
@@ -13,12 +14,19 @@ type Props = Record<string, never>;
 export default class MainPage extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    this.state = { query: localStorage.getItem(LS_QUERY_ITEM_NAME) ?? '' };
+    this.state = {
+      query: localStorage.getItem(LS_QUERY_ITEM_NAME) ?? '',
+      throwError: false,
+    };
   }
 
   onSearchSubmit = (query: string) => {
     localStorage.setItem(LS_QUERY_ITEM_NAME, query);
     this.setState({ query });
+  };
+
+  throwError = () => {
+    throw new Error('Test error');
   };
 
   render() {
@@ -27,6 +35,16 @@ export default class MainPage extends Component<Props, State> {
         <h1 className="my-6 text-3xl text-center">Hi there!</h1>
         <SearchBar query={this.state.query} onSubmit={this.onSearchSubmit} />
         <CardList query={this.state.query} />
+
+        {this.state.throwError && this.throwError()}
+        <button
+          className="fixed right-10 bottom-10 px-4 py-2 text-2xl text-black bg-red-700 rounded-full hover:text-white"
+          onClick={() => {
+            this.setState({ throwError: true });
+          }}
+        >
+          Throw Error
+        </button>
       </>
     );
   }
